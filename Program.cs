26208c52f585d2b8f11a9031afbebdf8b4cf8a34
TxcToxic <93470009+TxcToxic@users.noreply.github.com>
2023-03-12 22:04:50 +0100

    C# (CSharp)

---
+++ Program.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Net;
+
+namespace Discord_VoIP_Resolver
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Discord-VoIP-Name > ");
+            string serverName = Console.ReadLine();
+            try
+            {
+                IPAddress[] addresses = Dns.GetHostAddresses(serverName + ".discord.gg");
+                foreach (IPAddress ip in addresses)
+                {
+                    Console.WriteLine($"{serverName} > {ip}");
+                }
+            }
+            catch
+            {
+                Console.WriteLine($"Cannot resolve {serverName}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
