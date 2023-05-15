c94cda6d655f8c07d2a60e57b1e6bc8212a54a1b
-TOXIC- <93470009+TxcToxic@users.noreply.github.com>
2023-05-15 23:42:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import socket
 
-server_name = input("VOIP SRV NAME > ") + ".discord.media"
+server_name = input("VOIP SRV NAME > ")
+srv1 = server_name + ".discord.media"
+srv2 = server_name + ".discord.gg"
 
 try:
     ipaddr = socket.gethostbyname(server_name)
-    print(ipaddr)
+    ipaddr2 = socket.gethostbyname(server_name)
+    print(ipaddr + "/media")
+    print(ipaddr2 + "/gg")
 except:
     print("Failed to resolve")
