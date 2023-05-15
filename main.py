557f69a490a07d4a3710ca2cf6307e2a9fc10b4f
-TOXIC- <93470009+TxcToxic@users.noreply.github.com>
2023-05-15 23:38:30 +0200

    .gg -> .media
    sniffed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import socket
 
-server_name = input("VOIP SRV NAME > ") + ".discord.gg"
+server_name = input("VOIP SRV NAME > ") + ".discord.media"
 
 try:
     ipaddr = socket.gethostbyname(server_name)
